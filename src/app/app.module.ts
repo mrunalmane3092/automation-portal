4f88f8e192cd4801e18d221b2c07cc8b5e7e481e
aleenageorge <aleena@edcast.com>
2020-09-24 16:03:53 +0530

    modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,9 @@ import { ModalsComponent } from './shared/modals/modals.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/modals/modals.component.ts
+++ src/app/shared/modals/modals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modals',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  openModal(id) {
+    var modal = document.getElementById(id);
+    modal.style.display = "block";
+  }
+  
+  closeModal(id) {
+    var modal = document.getElementById(id);
+    modal.style.display = "none";
+  }
+
+  open(content) {
+    console.log('1')
+    this.modalService.open(content, {}).result.then((result) => {
+      // this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+
+    });
   }
 
 }
