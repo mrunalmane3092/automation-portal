60ad04f7f3c890280dff53bd8449ea339e768c06
aleenageorge <aleena@edcast.com>
2020-09-25 18:35:14 +0530

    update

--- src/app/sitelayout/sitelayout.component.ts
+++ src/app/sitelayout/sitelayout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 declare var $: any;
@@ -9,8 +9,8 @@ declare var $: any;
   styleUrls: ['./sitelayout.component.scss']
 })
 export class SitelayoutComponent implements OnInit {
-
-  leftPanelList = [
+  windowHeight = window.innerHeight;
+  newleftPanelList = [
     {
       "key": "Search",
       "component": "searchComponent",
@@ -47,13 +47,16 @@ export class SitelayoutComponent implements OnInit {
       "icon": "fa fa-window-restore"
     }
   ];
-
+  leftPanelList;
+  moreArray=[];
   constructor(
     private router: Router
   ) { }
 
   ngOnInit(): void {
     this.scrollIntoView('searchComponent');
+    this.leftPanelList = this.newleftPanelList;
+    this.screenSizeChange();
   }
 
   scrollIntoView(component) {
@@ -77,4 +80,37 @@ export class SitelayoutComponent implements OnInit {
     }, 0);
 
   }
+
+  
+  @HostListener('window:resize', ['$event'])
+  screenSizeChange() {
+
+    if (window.innerHeight - 50 < this.leftPanelList.length * 80) {
+      let viewItemTill = Math.floor((window.innerHeight - 50) / 80);
+      var array = [];
+      this.moreArray = [];
+      for (let i = 0; i < this.leftPanelList.length; i++) {
+        if (i < viewItemTill) {
+          array[i] = this.newleftPanelList[i];
+        } else {
+          this.moreArray.push(this.newleftPanelList[i])
+        }
+      }
+      this.leftPanelList = array;
+    } else {
+      this.moreArray = [];
+      let viewItemTill = Math.floor((window.innerHeight - 50) / 80);
+      while (this.leftPanelList.length < viewItemTill) {
+        this.leftPanelList.push(this.newleftPanelList[viewItemTill - 1])
+      }
+      for (let i = 0; i < this.newleftPanelList.length; i++) {
+        if (i > viewItemTill - 1) {
+          this.moreArray.push(this.newleftPanelList[i])
+        }
+
+      }
+    }
+
+  }
+  
 }
