ad29245ea01fbaf211e0181eb89f1cee31492993
mrunalmane3092 <mrunal@edcast.com>
2020-09-25 13:29:31 +0530

    shuffle divs

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-portal',
   templateUrl: './portal.component.html',
@@ -10,6 +10,14 @@ export class PortalComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    $(() => {
+      const parent = $('#portal_section');
+      const divs = parent.children();
+      while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+      }
+    });
   }
 
 }
