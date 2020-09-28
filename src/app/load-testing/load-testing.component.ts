05bb0a44f594435c6ea636a766a5f450404df358
aleenageorge <aleena@edcast.com>
2020-09-28 16:44:24 +0530

    update

--- src/app/load-testing/load-testing.component.ts
+++ src/app/load-testing/load-testing.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./load-testing.component.scss']
 })
 export class LoadTestingComponent implements OnInit {
-  array = new Array<number>(10);
+  array = new Array<number>(17);
   constructor() { }
 
   ngOnInit(): void {
